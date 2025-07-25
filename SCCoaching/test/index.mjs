c25bcfce700e6b3fbe1916d3b9e1c91b467da320
Manojkumar <g.manojkumar@p3fusion.com>
2025-07-25 07:57:43 +0000

    Sync raw Lambda files from S3

---
+++ SCCoaching/test/index.mjs
@@ -0,0 +1,10 @@
+import JSON
+export const handler = async (event) => {
+  // TODO implement
+  console.log(" asdsdaff update from local 7")
+  const response = {
+    statusCode: 200,
+    body: JSON.stringify('Hello from Lambda!'),
+  };
+  return response;
+};
